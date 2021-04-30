47439fafcc2009d686d869baf2d94344a4fb6728
Patrick Sawyer <patrick_sawyer@hotmail.co.uk>
2021-04-30 16:31:36 +0100

    test7

---
+++ build/controllers/recipients.d.ts
@@ -0,0 +1,2 @@
+import * as express from "express";
+export declare const recipientsController: express.Router;
---
+++ build/db.d.ts
@@ -0,0 +1,8 @@
+declare const _default: {
+    host: string | undefined;
+    database: string | undefined;
+    user: string | undefined;
+    password: string | undefined;
+    port: number;
+};
+export default _default;
